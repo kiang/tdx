0c2cd0cc00daa0ba0700e176ccc5f64e8fa575c2
kiang <kiange@gmail.com>
2021-10-13 14:32:11 +0800

    010 add single click to map

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,7 +12,7 @@ for (var z = 0; z < 20; ++z) {
   matrixIds[z] = z;
 }
 var sidebarTitle = document.getElementById('sidebarTitle');
-var content = document.getElementById('infoBox');
+var content = document.getElementById('sidebarContent');
 
 var appView = new ol.View({
   center: ol.proj.fromLonLat([120.721507, 23.700694]),
@@ -54,3 +54,20 @@ var map = new ol.Map({
 });
 
 map.addControl(sidebar);
+
+map.on('singleclick', function (evt) {
+  content.innerHTML = '';
+  pointClicked = false;
+  map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+    if (false === pointClicked) {
+      pointClicked = true;
+      var p = feature.getProperties();
+      for(k in p) {
+        if(k !== 'geometry') {
+          content.innerHTML += k + ': ' + p[k] + '<br />';
+        }
+      }
+      sidebar.open('home');
+    }
+  });
+});
