cf2081cf65be489813d11e87fc29f102b65b7214
kiang <kiange@gmail.com>
2021-10-13 14:19:06 +0800

    008 blank map

---
+++ docs/js/main.js
@@ -0,0 +1,47 @@
+var sidebar = new ol.control.Sidebar({ element: 'sidebar', position: 'right' });
+var jsonFiles, filesLength, fileKey = 0;
+
+var projection = ol.proj.get('EPSG:3857');
+var projectionExtent = projection.getExtent();
+var size = ol.extent.getWidth(projectionExtent) / 256;
+var resolutions = new Array(20);
+var matrixIds = new Array(20);
+for (var z = 0; z < 20; ++z) {
+  // generate resolutions and matrixIds arrays for this WMTS
+  resolutions[z] = size / Math.pow(2, z);
+  matrixIds[z] = z;
+}
+var sidebarTitle = document.getElementById('sidebarTitle');
+var content = document.getElementById('infoBox');
+
+var appView = new ol.View({
+  center: ol.proj.fromLonLat([120.721507, 23.700694]),
+  zoom: 9
+});
+
+var baseLayer = new ol.layer.Tile({
+  source: new ol.source.WMTS({
+    matrixSet: 'EPSG:3857',
+    format: 'image/png',
+    url: 'https://wmts.nlsc.gov.tw/wmts',
+    layer: 'EMAP',
+    tileGrid: new ol.tilegrid.WMTS({
+      origin: ol.extent.getTopLeft(projectionExtent),
+      resolutions: resolutions,
+      matrixIds: matrixIds
+    }),
+    style: 'default',
+    wrapX: true,
+    attributions: '<a href="http://maps.nlsc.gov.tw/" target="_blank">國土測繪圖資服務雲</a>'
+  }),
+  opacity: 0.8
+});
+
+
+var map = new ol.Map({
+  layers: [baseLayer],
+  target: 'map',
+  view: appView
+});
+
+map.addControl(sidebar);
